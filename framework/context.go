1214d412927fd764adf177264a6413906d5599f9
xuzhongwei <xu.zhongwei@ideabox-it.com>
2022-12-12 23:52:57 +0900

    Squashed commit of the following:
    commit b35f020215526f95320adb91d9c16766525cdc10
    Author: xuzhongwei <xu.zhongwei@ideabox-it.com>
    Date:   Mon Dec 12 23:49:38 2022 +0900
    
        Send
    
    commit 71776137ffd700bb7c4e8ca0f0a20a877c9b6ac7
    Author: xuzhongwei <xu.zhongwei@ideabox-it.com>
    Date:   Mon Dec 12 23:37:54 2022 +0900
    
        context
    
    commit 1a506e92a26070d572e00a37acc55d47e0bf1f35
    Author: xuzhongwei <xu1718191411@gmail.com>
    Date:   Sat Dec 10 12:33:36 2022 +0900
    
        skeleton for framework engine

---
+++ framework/context.go
@@ -0,0 +1,83 @@
+package framework
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type JolContext struct {
+	writer    http.ResponseWriter
+	request   *http.Request
+	mutax     sync.Mutex
+	isTimeout bool
+}
+
+func NewContext(writer http.ResponseWriter, request *http.Request) *JolContext {
+	return &JolContext{
+		writer:  writer,
+		request: request,
+		mutax:   sync.Mutex{},
+	}
+}
+
+func (c *JolContext) BaseContext() context.Context {
+	return c.request.Context()
+}
+
+func (c *JolContext) Done() <-chan struct{} {
+	return c.BaseContext().Done()
+}
+
+func (c *JolContext) Deadline() (deadline time.Time, ok bool) {
+	return c.BaseContext().Deadline()
+}
+
+func (c *JolContext) Err() error {
+	return c.BaseContext().Err()
+}
+
+func (c *JolContext) Value(key any) any {
+	return c.BaseContext().Value(key)
+}
+
+func (c *JolContext) Lock() {
+	c.mutax.Lock()
+}
+
+func (c *JolContext) UnLock() {
+	c.mutax.Unlock()
+}
+
+func (c *JolContext) setIsTimeout(v bool) {
+	c.isTimeout = v
+}
+
+func (c *JolContext) Send(text string) {
+	if c.isTimeout {
+		return
+	}
+
+	c.writer.WriteHeader(http.StatusOK)
+	c.writer.Header().Set("Content-Type", "text/plain")
+	c.writer.Write([]byte(text))
+	return
+}
+
+func (c *JolContext) Json(data any) {
+	if c.isTimeout {
+		return
+	}
+	byteData, err := json.Marshal(data)
+
+	if err != nil {
+		c.writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	c.writer.Header().Set("Content-Type", "application/json")
+	c.writer.WriteHeader(http.StatusOK)
+	c.writer.Write(byteData)
+	return
+}
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"JOLFramework/framework"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	engine := framework.Engine{}
+	port := 8080
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), &engine))
+	fmt.Println("listening on port:", port)
+}
